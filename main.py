f079ac5397e46f6c7d2a722c9bb55a25cd7437f3
kona358 <34427772+kona358@users.noreply.github.com>
2023-10-27 21:46:02 -1000

    Update main.py
    Steps toward making it work for general users

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ from spotipy.oauth2 import SpotifyOAuth
 
 
 scope = "playlist-modify-public"
-token = util.prompt_for_user_token('9h6llq95nilh7ifckssmamzmm',scope,client_id='14f23c99c6174bc7843cd7864abc22cc',client_secret='b2d3a6bbebe24e69a78b98a446471072',redirect_uri='https://localhost:8080')
+token = util.prompt_for_user_token(None,scope,client_id='14f23c99c6174bc7843cd7864abc22cc',client_secret='b2d3a6bbebe24e69a78b98a446471072',redirect_uri='https://localhost:8080')
 spotify = sp.Spotify(auth=token)
+
+# get user id
+user = spotify.me()['id']
+print(user)
+
 url ='https://spinitron.com/KTUH/pl/18030348/In-The-Garage'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -17,7 +22,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 elements = soup.find_all(class_='spin-item')
 
 #create a playlist
-playlist = spotify.user_playlist_create('In The Garage-Generated2', public=True, collaborative=False, description='')
+playlist = spotify.user_playlist_create(user, 'In The Garage-Generated2', public=True, collaborative=False, description='')
 
 songByArtist = []
 for element in elements:
