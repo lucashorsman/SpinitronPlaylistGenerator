3dc0bbe8fda97e4b45d4829a22261c452feea374
kona358 <34427772+kona358@users.noreply.github.com>
2023-10-30 16:42:24 -1000

    User Friendly Update
    Making the app usable for people unfamilar with Spotify API /Spotipy

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 import requests
 import spotipy as sp
@@ -7,42 +6,54 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
 
+def parse_url():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    elements = soup.find_all(class_="spin-item")
+    songs = []
+    for element in elements:
+        print(element.find(class_="artist").get_text())
+        print(element.find(class_="song").get_text())
+        songs.append(
+            element.find(class_="artist").get_text()
+            + " "
+            + element.find(class_="song").get_text()
+        )
+
+    return songs
+
+
+# main
+# general setup
+client_id = "14f23c99c6174bc7843cd7864abc22cc"
+client_secret = "b2d3a6bbebe24e69a78b98a446471072"
 scope = "playlist-modify-public"
-token = util.prompt_for_user_token(None,scope,client_id='14f23c99c6174bc7843cd7864abc22cc',client_secret='b2d3a6bbebe24e69a78b98a446471072',redirect_uri='https://localhost:8080')
+token = util.prompt_for_user_token(
+    None, scope, client_id, client_secret, redirect_uri="https://localhost:8080"
+)
 spotify = sp.Spotify(auth=token)
+user = spotify.me()["id"]
 
-# get user id
-user = spotify.me()['id']
-print(user)
-
-url ='https://spinitron.com/KTUH/pl/18030348/In-The-Garage'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-elements = soup.find_all(class_='spin-item')
-
-#create a playlist
-playlist = spotify.user_playlist_create(user, 'In The Garage-Generated2', public=True, collaborative=False, description='')
-
-songByArtist = []
-for element in elements:
-    print(element.find(class_='artist').get_text())
-    print(element.find(class_='song').get_text())
-    songByArtist.append(element.find(class_='artist').get_text() + ' ' + element.find(class_='song').get_text())
-
-    
+# user input
+url = input("Enter the Spinitron URL: ")
+playlistName = input("Enter the name of the playlist: ")
+description = input("Enter the description of the playlist(hit enter if you dont want to add a description): ")
+# create a playlist
+playlist = spotify.user_playlist_create(
+    user, playlistName, public=True, collaborative=False, description=description
+)
 
 songByArtistinSpotify = []
-
-
-for song in songByArtist:
-    result = spotify.search(q=song, type='track')
+songs = parse_url()
+for song in songs:
+    result = spotify.search(q=song, type="track")
     try:
-        uri = result['tracks']['items'][0]['uri']
+        uri = result["tracks"]["items"][0]["uri"]
         songByArtistinSpotify.append(uri)
     except IndexError:
         print(f"No results found for {song}")
 
-
-
-spotify.playlist_add_items(playlist_id=playlist['id'], items=songByArtistinSpotify, position=None)
+spotify.playlist_add_items(
+    playlist_id=playlist["id"], items=songByArtistinSpotify, position=None
+)
